5cafcfd52b7dc3ee297c5be1afb5efec5b453d89
nonstriater <410495266@qq.com>
2015-07-29 15:14:10 +0800

    fix reusacellId

--- Olla4iOS/application/controller/OllaTableDataController.m
+++ Olla4iOS/application/controller/OllaTableDataController.m
@@ -114,7 +114,11 @@
     
     CGFloat height = [self heightForRowAtIndexPath:indexPath];
     if(height<0.00001f){
-        height = [tableView fd_heightForCellWithIdentifier:self.reusableCellIdentifier cacheByIndexPath:indexPath configuration:^(OllaTableViewCell *cell){
+        NSString *reusableCellIdentifier = [self nibNameAtIndexPath:indexPath];
+        if ([reusableCellIdentifier length]==0) {
+            reusableCellIdentifier = self.itemViewNib;
+        }
+        height = [tableView fd_heightForCellWithIdentifier:reusableCellIdentifier cacheByIndexPath:indexPath configuration:^(OllaTableViewCell *cell){
             [self configCell:cell atIndexPath:indexPath];
         }];
         if (height<0.00001f) {
